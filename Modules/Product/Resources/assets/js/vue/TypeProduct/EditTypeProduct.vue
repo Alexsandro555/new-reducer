03a8377815c565e2d460c47e750dd508125c0e95
alexsandro <alexsandro@oooleader.ru>
2019-04-02 11:11:09 +0300

    update tp

--- Modules/Product/Resources/assets/js/vue/TypeProduct/EditTypeProduct.vue
+++ Modules/Product/Resources/assets/js/vue/TypeProduct/EditTypeProduct.vue
@@ -11,6 +11,7 @@
             <v-container>
               <v-layout row wrap>
                 <v-flex>
+                  {{fields}}
                   <v-form ref="form" lazy-validation v-model="valid">
                     <template v-for="(field, num) in fields">
                       <form-builder :field="field" v-if="num!=='description'" :relations="relations" :num="num" :items="form"
