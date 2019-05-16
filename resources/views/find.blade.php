9b2c3cef6810a7c6f7141c8a06003f708512cbf1
alexsandro <alexsandro@oooleader.ru>
2019-05-16 08:24:33 +0300

    update find elements

--- resources/views/find.blade.php
+++ resources/views/find.blade.php
@@ -12,45 +12,12 @@
   <div class="content">
     <div class="wrapper content__product-margin">
       <v-flex xs12 text-xs-left class="top-20 bottom-20">
-        @foreach($products as $product)
-        <div class="product-wrapper">
-          <div class="product">
-            <div class="product-image-wrapper">
-              <div class="product-image">
-                @if($product->files->count()>0)
-                  @foreach($product->files as $fileRecord)
-                    @foreach($fileRecord->config as $files)
-                      @foreach($files as $key => $file)
-                        @if($key == 'medium')
-                          <img src="/storage/{{$file['filename']}}"/>
-                        @endif
-                      @endforeach
-                    @endforeach
-                    @break
-                  @endforeach
-                @else
-                  <img src="{{asset('images/no-image-medium.png')}}"/>
-                @endif
-              </div>
-            </div>
-            <div class="product__title">
-              <a href="/catalog/detail/{{$product->url_key}}">
-                {{str_limit($product->title, $limit = 27, $end="...")}}
-              </a>
-            </div>
-            <v-layout row wrap>
-              <v-flex xs8 text-xs-center>
-                <span class="product-old-price">{{$product->price}} руб.</span><br>
-                <span class="product-price-wrapper">
-                    <span class="product-price">{{$product->price}}</span> руб.</span>
-              </v-flex>
-              <v-flex xs4>
-                <img src="{{asset('images/btn-sale.png')}}"/>
-              </v-flex>
-            </v-layout>
-          </div>
-        </div>
-        @endforeach
+        @if($products->isNotEmpty())
+          <h1>Результаты поиска</h1>
+          @include('products',['products' => $products])
+        @else
+          <h1>По вашему запросу ничего не найдено</h1>
+        @endif
       </v-flex>
     </div>
   </div>
