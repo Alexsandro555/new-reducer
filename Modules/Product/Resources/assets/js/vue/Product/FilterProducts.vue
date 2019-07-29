31099179413108c1e2693a37a216be6972b9f22f
alexsandro <alexsandro@oooleader.ru>
2019-07-29 09:33:33 +0300

    fdf

--- Modules/Product/Resources/assets/js/vue/Product/FilterProducts.vue
+++ Modules/Product/Resources/assets/js/vue/Product/FilterProducts.vue
@@ -12,7 +12,7 @@
                     :name="attribute.id+'_id'"
                     :label="attribute.title"
                     :items="attribute.attribute_list_value"
-                    :item-text="itemText(attribute)"
+                    :item-text="itemText"
                     item-value="id"
                     no-data-text="Нет данных"
                     :value="attribute.value"
